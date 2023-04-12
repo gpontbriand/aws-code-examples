94938e8b9dcf5978f6f3f499d8f0e857d744d6ec
Guillaume Pontbriand <107485180+gpontbriand@users.noreply.github.com>
2023-04-12 17:43:55 -0400

    Cleanup whitespace

--- kms/decrypt.sh
+++ kms/decrypt.sh
@@ -1,7 +1,7 @@
 aws kms decrypt \
     --ciphertext-blob fileb://secret_encrypted \
     --key-id alias/YOUR_KEY_ALIAS \
-	--query Plaintext \
-	--output text > secret_decrypted.base64
+    --query Plaintext \
+    --output text > secret_decrypted.base64
 
 base64 --decode --ignore-garbage secret_decrypted.base64 > secret_decrypted
